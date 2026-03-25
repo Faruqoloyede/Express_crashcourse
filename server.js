02685d4174ffee94847c00529b0d6621181d89a6
Faruqoloyede <oloyedefaruq2@gmail.com>
2026-03-25 18:02:28 +0100

    create first api

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 8000;
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+let posts = [
+    {id:1, title: 'Post One'},
+    {id:2, title: 'Post Two'},
+    {id:3, title: 'Post Three'},
+]
 
-app.get('/about', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// setup static folder
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/posts', (req, res)=>{
+    res.json(posts);
 })
 
-app.listen(8000, ()=>{
-    console.log(`server is running on port 8000`)
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
 })
