21d1aa7e944936e47d0e9c48581378cc3b117562
Faruqoloyede <oloyedefaruq2@gmail.com>
2026-03-27 09:30:42 +0100

    created a folder for routes

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const posts = require('./routes/posts');
 const port = process.env.PORT || 8000;
 const app = express();
 
-let posts = [
-    {id:1, title: 'Post One'},
-    {id:2, title: 'Post Two'},
-    {id:3, title: 'Post Three'},
-]
 
 
-// get all posts
-app.get('/api/posts', (req, res)=>{
-    const limit = parseInt(req.query.limit)
-    if(!isNaN(limit) && limit > 0){
-        res.status(200).json(posts.slice(0,limit));
-    }else{
-        res.status(200).json(posts);
-    }
-});
-
-// get single posts
-app.get('/api/posts/:id', (req, res)=>{
-    const id = parseInt(req.params.id);
-    const post = posts.find((post)=> post.id === id);
-    if(!post){
-        res.status(404).json({messahe: `Post with id ${id} not found`})
-    }else{
-        res.status(200).json(post);
-    }
-})
-
+// Routes
+app.use('/api/posts', posts)
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`)
